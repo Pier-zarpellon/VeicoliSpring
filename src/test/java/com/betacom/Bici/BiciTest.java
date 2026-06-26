e759eed20175f3c9b6ba3cde683184ff6a675d58
garatomarco <garatoma@gmail.com>
2026-06-26 10:56:21 +0200

    primo aumento coverage

--- src/test/java/com/betacom/Bici/BiciTest.java
+++ src/test/java/com/betacom/Bici/BiciTest.java
@@ -17,8 +17,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import com.betacom.controllers.BiciController;
 import com.betacom.dto.input.BiciReq;
 import com.betacom.dto.output.ResponseDTO;
+import com.betacom.repositories.ITipoVeicoloRepositories;
 
 import lombok.extern.slf4j.Slf4j;
 import tools.jackson.databind.ObjectMapper;
@@ -36,7 +38,12 @@ public class BiciTest {
 	@Autowired
 	private ObjectMapper objectMapper;
 	
-
+	@Autowired
+	private BiciController biciC;
+	
+	@Autowired
+	private ITipoVeicoloRepositories tipoVeicoloRepository;
+	
 	
 	@Test
 	@Order(1)
@@ -116,9 +123,149 @@ public class BiciTest {
 		
 	}
 	
-
+	@Test
+	@Order(4)
+	public void createBiciTestError2() throws Exception{
+		log.debug("createBiciTestError2");
+		BiciReq req = new BiciReq();
+		req.setAlimentazioneId(1);
+		req.setNumeroMarce(2);
+		req.setNumeroRuote(2);
+		req.setColore("bianco");
+		req.setMarca("ktm");
+		req.setModello("d4");
+		req.setCategoriaId(1);
+		req.setFrenoId(1);
+		req.setPieghevole(false);
+		req.setSospensioneId(1);
+		req.setTipoVeicoloId(1);
+		
+		MvcResult result =  mockMvc.perform(post("/rest/bici/create")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(req))
+				)
+		.andExpect(status().isBadRequest())		 
+		.andReturn();
+		
+		String json = result.getResponse().getContentAsString();
+		ResponseDTO dto = objectMapper.readValue(json, ResponseDTO.class);
+		
+		log.debug("rc create :{}", dto.getMsg());
+		
+	}
+	
 	@Test
 	@Order(5)
+	public void updateBiciTestError() throws Exception{
+		log.debug("updateBiciTestError");
+		BiciReq req = new BiciReq();
+		req.setIdBici(1);
+		req.setCategoriaId(10);
+		
+		MvcResult result =  mockMvc.perform(post("/rest/bici/update")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(req))
+				)
+		.andExpect(status().isBadRequest())		 
+		.andReturn();
+		
+		String json = result.getResponse().getContentAsString();
+		ResponseDTO dto = objectMapper.readValue(json, ResponseDTO.class);
+		
+		log.debug("rc update :{}", dto.getMsg());
+		
+	}
+	
+	@Test
+	@Order(6)
+	public void updateBiciTestError2() throws Exception{
+		log.debug("updateBiciTestError");
+		BiciReq req = new BiciReq();
+		req.setCategoriaId(1);;
+		
+		MvcResult result =  mockMvc.perform(post("/rest/bici/update")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(req))
+				)
+		.andExpect(status().isBadRequest())		 
+		.andReturn();
+		
+		String json = result.getResponse().getContentAsString();
+		ResponseDTO dto = objectMapper.readValue(json, ResponseDTO.class);
+		
+		log.debug("rc update :{}", dto.getMsg());
+		
+	}
+	
+	@Test
+	@Order(7)
+	public void updateBiciTestError3() throws Exception{
+		log.debug("updateBiciTestError");
+		BiciReq req = new BiciReq();
+		req.setIdBici(1);
+		req.setAlimentazioneId(10);
+		
+		MvcResult result =  mockMvc.perform(post("/rest/bici/update")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(req))
+				)
+		.andExpect(status().isBadRequest())		 
+		.andReturn();
+		
+		String json = result.getResponse().getContentAsString();
+		ResponseDTO dto = objectMapper.readValue(json, ResponseDTO.class);
+		
+		log.debug("rc update :{}", dto.getMsg());
+		
+	}
+	
+	@Test
+	@Order(8)
+	public void updateBiciTestError4() throws Exception{
+		log.debug("updateBiciTestError");
+		BiciReq req = new BiciReq();
+		req.setIdBici(1);
+		req.setSospensioneId(10);
+		
+		MvcResult result =  mockMvc.perform(post("/rest/bici/update")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(req))
+				)
+		.andExpect(status().isBadRequest())		 
+		.andReturn();
+		
+		String json = result.getResponse().getContentAsString();
+		ResponseDTO dto = objectMapper.readValue(json, ResponseDTO.class);
+		
+		log.debug("rc update :{}", dto.getMsg());
+		
+	}
+	
+	@Test
+	@Order(9)
+	public void updateBiciTestError5() throws Exception{
+		log.debug("updateBiciTestError");
+		BiciReq req = new BiciReq();
+		req.setIdBici(1);
+		req.setFrenoId(10);
+		
+		MvcResult result =  mockMvc.perform(post("/rest/bici/update")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(req))
+				)
+		.andExpect(status().isBadRequest())		 
+		.andReturn();
+		
+		String json = result.getResponse().getContentAsString();
+		ResponseDTO dto = objectMapper.readValue(json, ResponseDTO.class);
+		
+		log.debug("rc update :{}", dto.getMsg());
+		
+	}
+	
+
+	@Test
+	@Order(10)
 	public void deleteBici() throws Exception{
 		log.debug("deleteBici");
 		
